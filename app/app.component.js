232ff3ba58a358bdfdaec186ddc1be8ec0ad75e5
kirandash <thekirankumardash@gmail.com>
2017-04-14 22:05:41 +0530

    Use Components

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './courses.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,22 +10,27 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, courses_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (courses_component_1_1) {
+                courses_component_1 = courses_component_1_1;
             }],
         execute: function() {
-            //Decorator
+            // Decorator fn calling the object and using an object
             AppComponent = (function () {
                 function AppComponent() {
                 }
                 AppComponent = __decorate([
+                    // path of the module containing courses component
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>Hello Angular</h1>'
+                        template: '<h1>Hello Angular</h1><courses></courses>',
+                        directives: [courses_component_1.CoursesComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,14 @@
+// Importing component decorator from angular
 import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component'; // path of the module containing courses component
 
-//Decorator
+// Decorator fn calling the object and using an object
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello Angular</h1>'
+    template: '<h1>Hello Angular</h1><courses></courses>',
+    directives: [CoursesComponent]
+    // Directive - a class that allows us to extend of control DOM (and not part of standard html)
 })
+
+// export class - AppComponent which is the root of our application - takes control of entire app/page
 export class AppComponent { }

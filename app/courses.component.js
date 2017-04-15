5216f3e55272224e2fa9f1b4aa1c5029c395f266
kirandash <thekirankumardash@gmail.com>
2017-04-15 22:07:33 +0530

    Dependency Injection

--- app/courses.component.js
+++ app/courses.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './course.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,28 +10,34 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, course_service_1;
     var CoursesComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (course_service_1_1) {
+                course_service_1 = course_service_1_1;
             }],
         execute: function() {
             // Call the decorator - with @ before it. All decorators are fns so they must be called
             CoursesComponent = (function () {
-                function CoursesComponent() {
+                // reference the CourseService
+                function CoursesComponent(courseService) {
                     // a component decorator is used to tell angular that this class is a component
                     this.title = "The title of courses page";
-                    this.courses = ["Course1", "Course2", "Course3"];
+                    // dependency injection takes a class and injects a reference to it in the coursescomponent
+                    this.courses = courseService.getCourses();
                 }
                 CoursesComponent = __decorate([
-                    // In angular2 core module there is a Component decorator - so it is included
                     core_1.Component({
                         selector: 'courses',
-                        template: "\n\t\t\t\t<h2>Courses</h2>\n\t\t\t\t{{ title }}\n\t\t\t\t<ul>\n\t\t\t\t\t<li *ngFor=\"#course of courses\">\n\t\t\t\t\t{{ course }}\n\t\t\t\t\t</li>\n\t\t\t\t</ul>\n\t\t\t  " // template for the component - back tick allows us to have multiple lines
+                        template: "\n\t\t\t\t<h2>Courses</h2>\n\t\t\t\t{{ title }}\n\t\t\t\t<ul>\n\t\t\t\t\t<li *ngFor=\"#course of courses\">\n\t\t\t\t\t{{ course }}\n\t\t\t\t\t</li>\n\t\t\t\t</ul>\n\t\t\t  ",
+                        // ngFor to iterate through object
+                        providers: [course_service_1.CourseService]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [course_service_1.CourseService])
                 ], CoursesComponent);
                 return CoursesComponent;
             }());
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,4 +1,6 @@
 import {Component} from 'angular2/core'; // In angular2 core module there is a Component decorator - so it is included
+// import CourseService from course.service.ts
+import {CourseService} from './course.service';
 
 // Call the decorator - with @ before it. All decorators are fns so they must be called
 @Component({
@@ -11,8 +13,9 @@ import {Component} from 'angular2/core'; // In angular2 core module there is a C
 					{{ course }}
 					</li>
 				</ul>
-			  ` // template for the component - back tick allows us to have multiple lines
+			  `, // template for the component - back tick allows us to have multiple lines
 			  // ngFor to iterate through object
+	providers: [CourseService]
 })
 
 // Create a class
@@ -20,5 +23,11 @@ import {Component} from 'angular2/core'; // In angular2 core module there is a C
 export class CoursesComponent {
 	// a component decorator is used to tell angular that this class is a component
 	title = "The title of courses page";
-	courses = ["Course1", "Course2", "Course3"];
+	courses;
+
+	// reference the CourseService
+	constructor(courseService: CourseService){
+		// dependency injection takes a class and injects a reference to it in the coursescomponent
+		this.courses = courseService.getCourses();
+	}
 }

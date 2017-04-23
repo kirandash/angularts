f89a4cf0ed033f482b5ca4ec60266a91fa918e26
kirandash <thekirankumardash@gmail.com>
2017-04-23 18:43:15 +0530

    4 minlength maxlength and required subscription form

--- 4templatedrivenform/app/app.component.ts
+++ 4templatedrivenform/app/app.component.ts
@@ -2,12 +2,15 @@ import {Component} from 'angular2/core';
 
 import {ContactFormComponent} from './contact-form.component';
 
+import {SubscriptionFormComponent} from './subscription-form.component';
+
 @Component({
     selector: 'my-app',
-    directives: [ContactFormComponent],
+    directives: [ContactFormComponent, SubscriptionFormComponent],
     template: `
 		<div class="container">
 			<contact-form></contact-form>
+			<subscription-form></subscription-form>
 		</div>
     `
 })

0c877b177ed358d4c34511bc71f54d2865ddbf45
kirandash <thekirankumardash@gmail.com>
2017-04-24 22:30:09 +0530

    5 Create controls explicitly with model driven forms

--- 4templatedrivenform/app/signup-form.component.ts
+++ 4templatedrivenform/app/signup-form.component.ts
@@ -1,8 +1,18 @@
 import {Component} from 'angular2/core';
 
+import {ControlGroup, Control, Validators} from 'angular2/common';
+
 @Component({
     selector: 'signup-form',
     templateUrl: 'app/signup-form.component.html'
 })
 export class SignUpFormComponent {
+	// Model Driven forms (Creating control explicitly whereas in template driven forms the controls are created in template implicitly)
+	form = new ControlGroup({
+		username: new Control('', Validators.required),
+		password: new Control('', Validators.required) // default value, validators object
+	});
+	signup(){
+		console.log(this.form.value);
+	}
 }

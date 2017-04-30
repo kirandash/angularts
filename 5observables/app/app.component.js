10e5b03cb168f74bcb2056fbf695fd652c84e6cd
kirandash <thekirankumardash@gmail.com>
2017-04-30 13:47:01 +0530

    6 Create observable and subscribe to it

--- 5observables/app/app.component.js
+++ 5observables/app/app.component.js
@@ -1,5 +1,7 @@
 /// <reference path="../typings/tsd.d.ts" />
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', 'rxjs/Rx'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -9,16 +11,36 @@ System.register(['angular2/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, Rx_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (Rx_1_1) {
+                Rx_1 = Rx_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    /*var debounced = _.debounce(function(){
+                        var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
+                        $.getJSON(url, function(artists){
+                            console.log(artists);
+                        });
+                    }, 400);
+             
+                    $("#search").keyup(function(e){
+                        var text = e.target.value;
+                        if(text.length<3)
+                            return;
+                        debounced(text);
+                    });*/
+                    //new Observable();
+                    var keyups = Rx_1.Observable.fromEvent($("#search"), "keyup"); // Create static observable
+                    // subscribe to the observables
+                    keyups.subscribe(function (data) { return console.log(data); });
                 }
                 AppComponent = __decorate([
                     core_1.Component({
@@ -28,7 +50,7 @@ System.register(['angular2/core'], function(exports_1) {
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
-            })();
+            }());
             exports_1("AppComponent", AppComponent);
         }
     }
--- 5observables/app/app.component.ts
+++ 5observables/app/app.component.ts
@@ -1,6 +1,8 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import {Component} from 'angular2/core';
+// 
+import {Observable} from 'rxjs/Rx';
 
 @Component({
     selector: 'my-app',
@@ -10,18 +12,23 @@ import {Component} from 'angular2/core';
 })
 export class AppComponent {
     constructor(){
-    	var debounced = _.debounce(function(){
+    	/*var debounced = _.debounce(function(){
     		var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
     		$.getJSON(url, function(artists){
     			console.log(artists);
     		});    		
     	}, 400);
-
+ 
     	$("#search").keyup(function(e){
     		var text = e.target.value;
     		if(text.length<3)
     			return;
     		debounced(text);
-    	});
+    	});*/
+    	
+    	//new Observable();
+    	var keyups = Observable.fromEvent($("#search"), "keyup"); // Create static observable
+    	// subscribe to the observables
+    	keyups.subscribe(data => console.log(data));
     }
 }

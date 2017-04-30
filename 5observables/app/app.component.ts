554c76858788458b019e91efdd1906c9cfc63788
kirandash <thekirankumardash@gmail.com>
2017-04-30 12:30:23 +0530

    6 callback observable hell

--- 5observables/app/app.component.ts
+++ 5observables/app/app.component.ts
@@ -10,5 +10,18 @@ import {Component} from 'angular2/core';
 })
 export class AppComponent {
     constructor(){
+    	var debounced = _.debounce(function(){
+    		var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
+    		$.getJSON(url, function(artists){
+    			console.log(artists);
+    		});    		
+    	}, 400);
+
+    	$("#search").keyup(function(e){
+    		var text = e.target.value;
+    		if(text.length<3)
+    			return;
+    		debounced(text);
+    	});
     }
 }

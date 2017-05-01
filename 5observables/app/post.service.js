96ed95b51cbfd52c4ec432c4e379758bdd7023a7
kirandash <thekirankumardash@gmail.com>
2017-05-01 17:04:20 +0530

    7 Send data to server

--- 5observables/app/post.service.js
+++ 5observables/app/post.service.js
@@ -9,10 +9,15 @@ System.register(['rxjs/add/operator/map'], function(exports_1, context_1) {
             PostService = (function () {
                 function PostService(_http) {
                     this._http = _http;
+                    this._url = 'https://jsonplaceholder.typicode.com/posts';
                 }
                 PostService.prototype.getPost = function () {
-                    return this._http.get("https://jsonplaceholder.typicode.com/posts")
+                    return this._http.get(this._url)
                         .map(function (res) { return res.json(); }); // http returns Observable which is then mapped to get json value
+                };
+                PostService.prototype.createPost = function (post) {
+                    return this._http.post(this._url, JSON.stringify(post))
+                        .map(function (res) { return res.json(); });
                 };
                 return PostService;
             }());
--- 5observables/app/post.service.ts
+++ 5observables/app/post.service.ts
@@ -2,13 +2,18 @@ import {Http} from 'angular2/http';
 import 'rxjs/add/operator/map';
 
 export class PostService {
-	
+	private _url = 'https://jsonplaceholder.typicode.com/posts';
 	constructor(private _http: Http){
 
 	}
 
 	getPost(){
-		return this._http.get("https://jsonplaceholder.typicode.com/posts")
+		return this._http.get(this._url)
 			.map(res => res.json()); // http returns Observable which is then mapped to get json value
+	}
+
+	createPost(post){
+		return this._http.post(this._url, JSON.stringify(post))
+				.map(res => res.json());
 	}
 }

e68dd0a4edb5c270be807eed4555baf6a31d1455
kirandash <thekirankumardash@gmail.com>
2017-05-03 23:17:58 +0530

    8 Impoerative routing for submit button with Router.navigate()

--- 6spa/app/contact.component.ts
+++ 6spa/app/contact.component.ts
@@ -1,10 +1,16 @@
 import {Component} from 'angular2/core';
+import {Router from 'angular2/router';
 
 @Component({
     templateUrl: '/app/contact.component.html'
 })
 export class ContactComponent {
+    constructor(private _router: Router){
+
+    }
     onSubmit(form){
         console.log(form);
+        // Imperative Navigation with Router.navigate since routerLink can't be used here
+        this._router.navigate(['Albums']);
     }
 }

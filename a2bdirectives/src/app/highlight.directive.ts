29ac23d1e3fcb53d816415e58b41bbeb8b96e847
kirandash <thekirankumardash@gmail.com>
2017-05-16 23:15:37 +0530

    10 Use HostListener and HostBinding to interact with element

--- a2bdirectives/src/app/highlight.directive.ts
+++ a2bdirectives/src/app/highlight.directive.ts
@@ -1,4 +1,6 @@
-import { Directive, Input, ElementRef, Renderer } from '@angular/core';
+// import { Directive, Input, ElementRef, Renderer } from '@angular/core';
+
+import { Directive, Input, HostListener, HostBinding } from '@angular/core';
 
 // Custom directive
 @Directive({
@@ -8,12 +10,27 @@ export class HighlightDirective {
 
   @Input() dirHighlight: string; // When wrapping a property in brackets [] you're trying to bind to it. So you have to declare it as an @Input.
 
+  // HostListener and HostBinding meta data
+  @HostListener('mouseenter') mouseover(){
+  	this.backgroundColor = 'green';
+  };
+
+  @HostListener('mouseleave') mouseleave(){
+  	this.backgroundColor = 'white';
+  };
+
+  @HostBinding('style.backgroundColor') get setColor() {
+  	return this.backgroundColor;
+  	// add backgroundcolor to style of the target
+  }
+
+  private backgroundColor = 'white';
   // private elementRef: ElementRef; // Private to limit it only to this class
-  constructor(private elementRef: ElementRef, private renderer: Renderer) {
+  /*constructor(private elementRef: ElementRef, private renderer: Renderer) {
   	// dependency injection to pass a reference of the current class to the element div where the directive is going to be bind
   	//this.elementRef.nativeElement.style.backgroundColor = 'green'; //nativeElement will return the html where the directive is bound
   	// This is not recommended and recommended to use renderer
   	this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
   }
-
+  */
 }

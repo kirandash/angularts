82b1a6df8dc8919d4211fd17a1a2b9d5e528fec0
kirandash <thekirankumardash@gmail.com>
2017-06-03 22:46:32 +0530

    Submit the form and use output in template-driven output

--- a2forms/src/app/template-driven/template-driven.component.ts
+++ a2forms/src/app/template-driven/template-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: 'template-driven.component.html'
 })
 export class TemplateDrivenComponent {
+	onSubmit(form: NgForm) { // form data received from local variable and ngForm directive which is of type NgForm
+		// console.log('This form works');
+		console.log(form);
+	}
 }

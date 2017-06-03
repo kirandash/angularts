2e9f0bbc667d3206de1b5806d7d21bf5e90dd2ad
kirandash <thekirankumardash@gmail.com>
2017-06-03 23:10:25 +0530

    Form valid or invalid states and classes

--- a2forms/src/app/template-driven/template-driven.component.ts
+++ a2forms/src/app/template-driven/template-driven.component.ts
@@ -4,7 +4,12 @@ import { NgForm } from '@angular/forms';
 @Component({
   moduleId: module.id,
   selector: 'template-driven',
-  templateUrl: 'template-driven.component.html'
+  templateUrl: 'template-driven.component.html',
+  styles: [`
+  	.ng-invalid {
+  		border: 1px solid red;
+  	}
+  `]
 })
 export class TemplateDrivenComponent {
 	onSubmit(form: NgForm) { // form data received from local variable and ngForm directive which is of type NgForm

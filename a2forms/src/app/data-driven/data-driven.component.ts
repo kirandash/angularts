d66983b3b5f6cbe6a8236d0f6bfc6fb111ced19e
kirandash <thekirankumardash@gmail.com>
2017-06-04 12:14:41 +0530

    11 - 11 create data driven form with FormGroup and FormControl

--- a2forms/src/app/data-driven/data-driven.component.ts
+++ a2forms/src/app/data-driven/data-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
     templateUrl: 'data-driven.component.html'
 })
 export class DataDrivenComponent {
-    
+    myForm: FormGroup; // creating the form using FormGroup unlike default angular template driven approach
+    // defining the type of myForm variable as FormGroup
+    constructor() {
+    	this.myForm = new FormGroup({
+    		'username': new FormControl(),// in template driven approach form control was automatically created by adding ngModel directive to input
+    		'email': new FormControl(),
+    		'password': new FormControl()
+    	});
+    }// note that for simpler forms constructor can be used bt for heavy complex forms, ngOnInit can be used
 }

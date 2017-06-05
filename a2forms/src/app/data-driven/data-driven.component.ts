7c78901faf9c6eb3dfe337fd3d956fefb7031611
kirandash <thekirankumardash@gmail.com>
2017-06-05 22:54:21 +0530

    11 - 14 Input validation with Validators

--- a2forms/src/app/data-driven/data-driven.component.ts
+++ a2forms/src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -11,9 +11,9 @@ export class DataDrivenComponent {
     // defining the type of myForm variable as FormGroup
     constructor() {
     	this.myForm = new FormGroup({
-    		'username': new FormControl(),// in template driven approach form control was automatically created by adding ngModel directive to input
-    		'email': new FormControl(),
-    		'password': new FormControl()
+    		'username': new FormControl('kirandash', Validators.required),// in template driven approach form control was automatically created by adding ngModel directive to input
+    		'email': new FormControl('kiran@xhtmlchamps.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+    		'password': new FormControl('a345@30', Validators.required)// default value, validator, or array of validators
     	});
     }// note that for simpler forms constructor can be used bt for heavy complex forms, ngOnInit can be used
 
